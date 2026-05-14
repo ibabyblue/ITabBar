7f0f956c23866a8f5ead2299e7c944ac67981623
zhangbiao <ibabyblue_z@icloud.com>
2026-05-14 21:14:14 +0800

    chore: scaffold ITabBar SPM package

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "ITabBar",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(name: "ITabBar", targets: ["ITabBar"]),
+    ],
+    targets: [
+        .target(name: "ITabBar"),
+        .testTarget(name: "ITabBarTests", dependencies: ["ITabBar"]),
+    ]
+)
